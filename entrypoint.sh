19805ef1f58f7ac18760de5db15f21c3753bb3cb
Cash Myers <cmyers880@gmail.com>
2022-02-03 13:53:42 -0600

    feat: Add superuser allow for Admin on container. May need this if Adminer can't/won't work with volumes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
 set -e  # exit if errors happen anywhere
+export DJANGO_SUPERUSER_USERNAME=admin
+export DJANGO_SUPERUSER_PASSWORD=password1561
+export DJANGO_SUPERUSER_EMAIL=nomail@email.com
+
+python manage.py createsuperuser --noinput
+
 python ./drf_jwt_backend/manage.py migrate
 
 python ./drf_jwt_backend/manage.py runserver 0.0.0.0:8000
