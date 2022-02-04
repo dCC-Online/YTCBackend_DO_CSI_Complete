d561c5f8be84d2bbc2e85580203777f7760c7715
Cash Myers <cmyers880@gmail.com>
2022-02-04 11:51:30 -0600

    doc(cicd):Added internal commenting

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
 set -e  # exit if errors happen anywhere
+
+# Setup environment variables for Super User (will extract to be hidden later)
 export DJANGO_SUPERUSER_USERNAME=admin
 export DJANGO_SUPERUSER_PASSWORD=password1561
 export DJANGO_SUPERUSER_EMAIL=nomail@email.com
 
-python manage.py createsuperuser --noinput
-
+# The next steps must be executed in the order they are listed here.
 python ./drf_jwt_backend/manage.py migrate
+
+python ./drf_jwt_backend/manage.py createsuperuser --noinput
 
 python ./drf_jwt_backend/manage.py runserver 0.0.0.0:8000
